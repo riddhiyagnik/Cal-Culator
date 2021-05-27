d8d6bdced6d84f3e1f4e187f96a431180e3ff6ef
Riddhi <yagnikriddhi1@gmail.com>
2021-05-27 19:26:27 +0530

    modified few files

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -1,34 +1,34 @@
-let screen = document.getElementById('screen');
-buttons = document.querySelectorAll('button');
-let screenValue = '';
-let history = [];
+var screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+var screenValue = "";
+var CalHistory = [];
 for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
-            buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
-            
-        }
-        else if (buttonText == 'C') {
-            screenValue = "";
-            screen.value = screenValue;
-           
-        }
-        else if (buttonText == '=') {
-            screen.value = eval(screenValue);
-            var calculation = {ques: screenValue, ans: screen.value}
-          history.push(calculation)
-        }
-        else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-  
-    })
-    
+  // addEventListener() - attaches an event handler to an element//
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log("Button text is ", buttonText);
+    document.getElementById("History").innerHTML += buttonText;
+
+    if (buttonText == "x") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+    // 'C' - To clear the screen//
+    else if (buttonText == "C") {
+      screenValue = "";
+      screen.value = screenValue;
+    }
+    //To evaluate and return the element with ID attribute//
+    else if (buttonText == "=") {
+      screen.value = eval(screenValue);
+      var calculation = { ques: screenValue, ans: screen.value };
+      CalHistory.push(calculation);
+      console.log(CalHistory);
+      document.getElementById("History").innerHTML += screen.value + "<br>";
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
 }
-
-
